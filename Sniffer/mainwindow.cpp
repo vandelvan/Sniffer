c31befa8a3cfd930e947472445f3894c9f97a782
vandelvan <vandel_van@hotmail.com>
2021-03-05 12:15:52 -0600

    #1 Lectura de bin

--- Sniffer/mainwindow.cpp
+++ Sniffer/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+using namespace std;
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +15,20 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_buscarArchivo_clicked()
+{
+    QMessageBox errorMsg;
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Abrir bin"), "~/", tr("*.bin"));
+    ui->seleccionArchivo->setText(fileName);
+    QFile file(fileName);
+    if(!file.open(QIODevice::ReadOnly)){    //Si no se puede abrir
+        errorMsg.setText("Error al abrir "+ fileName + "!");
+        errorMsg.exec();
+        return;
+    }
+    QByteArray byteArray;
+    byteArray = file.readAll(); //toma el contenido del bin
+    file.close();
+    ui->dumpArchivo->setPlainText(byteArray.toHex());
+}
