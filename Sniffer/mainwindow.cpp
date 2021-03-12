caea2823d116ac1aece1437d03cc8b2c122acfa3
vandelvan <vandel_van@hotmail.com>
2021-03-12 11:55:39 -0600

    Refactor

--- Sniffer/mainwindow.cpp
+++ Sniffer/mainwindow.cpp
@@ -15,73 +15,27 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-QString MainWindow::ethernetTypeCode(QString tipo)
-{
-    if(tipo=="0800")
-    {
-        return "IPv4";
-    }
-    else if(tipo=="0806")
-    {
-        return "ARP";
-    }
-    else if(tipo=="8035")
-    {
-        return "RARP";
-    }
-    else if(tipo=="86DD")
-    {
-        return "IPv6";
-    }
-    else
-    {
-        return "Invalid Type";
-    }
-}
-
 void MainWindow::on_buscarArchivo_clicked()
 {
     std::locale loc;
-    QMessageBox errorMsg;
     QString fileName = QFileDialog::getOpenFileName(this, tr("Abrir bin"), "~/", tr("*.bin"));
     ui->seleccionArchivo->setText(fileName);
-    QFile file(fileName);
-    if(!file.open(QIODevice::ReadOnly)){    //Si no se puede abrir
-        errorMsg.setText("Error al abrir "+ fileName + "!");
-        errorMsg.exec();
-        return;
-    }
-    QByteArray byteArray;
-    byteArray = file.readAll(); //toma el contenido del bin
-    file.close();
+    QByteArray byteArray = reader.readFile(fileName);
+    if(byteArray == nullptr) return;
+
     QString macD="",macO="",tipo="",datos="";
     string dump=byteArray.toHex().toStdString();
-    int i;
-    for(i=0;i<12;i++)
+    macD = splitter.macDestino(dump);
+    macO = splitter.macOrigen(dump);
+    for(int i=24;i<28;i++)
     {
-        macD+=std::toupper(dump[i],loc);
-        if((i+1)%2==0 && i!=11)
-        {
-            macD+=":";
-        }
+        tipo+=toupper(dump[i],loc);
     }
-    for(i=12;i<24;i++)
-    {
-        macO+=std::toupper(dump[i],loc);
-        if((i+1)%2==0 && i!=23)
-        {
-            macO+=":";
-        }
-    }
-    for(i=24;i<28;i++)
-    {
-        tipo+=std::toupper(dump[i],loc);
-    }
-    tipo=tipo+" "+ethernetTypeCode(tipo);
-    i=28;
+    tipo=tipo+" "+splitter.ethernetTypeCode(tipo);
+    int i=28;
     while(dump[i])
     {
-        datos+=std::toupper(dump[i],loc);
+        datos+=toupper(dump[i],loc);
         i++;
         if(i%2==0)
         {
