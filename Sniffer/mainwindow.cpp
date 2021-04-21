66417405c2f76675ab05ee86df1b32cc82a06818
vandelvan <vandel_van@hotmail.com>
2021-04-20 19:54:27 -0500

    Correccion MAC

--- Sniffer/mainwindow.cpp
+++ Sniffer/mainwindow.cpp
@@ -49,6 +49,10 @@ void MainWindow::showEthernet(string dump){
     ui->datosPaquete->show();
     ui->macDestinoTxt->setText(splitter.macDestino(dump));
     ui->macOrigenTxt->setText(splitter.macOrigen(dump));
+    ui->macDestinoLbl->show();
+    ui->macDestinoTxt->show();
+    ui->macOrigenLbl->show();
+    ui->macOrigenTxt->show();
     ui->etherTypeTxt->setText(tipo);
     if(tipo == "0800 IPv4"){
         ui->datosEthernet->hide();
